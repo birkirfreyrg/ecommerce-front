2f227f9efa0450474d312bc9674fd7d501964661
birkirfreyrg <birkirfrey@msn.com>
2023-08-20 02:11:06 +0000

    Featured Product finished, NewProduct list finished.

--- components/Featured.js
+++ components/Featured.js
@@ -1,62 +1,74 @@
-import { styled } from "styled-components";
-import Center from "./Center";
-import Button from "./Button";
+import styled from "styled-components";
+import Center from "@/components/Center";
+import Button from "@/components/Button";
+import ButtonLink from "@/components/ButtonLink";
+import CartIcon from "./icons/CartIcon";
 
 const Bg = styled.div`
   background-color: #222;
   color: #fff;
   padding: 50px 0;
 `;
-
 const Title = styled.h1`
   margin: 0;
   font-weight: normal;
+  font-size: 1.5rem;
+  @media screen and (min-width: 768px) {
+    font-size: 3rem;
+  }
 `;
-
 const Desc = styled.p`
   color: #aaa;
   font-size: 0.8rem;
 `;
-
 const ColumnsWrapper = styled.div`
   display: grid;
-  grid-template-columns: 0.9fr 1.1fr;
+  grid-template-columns: 1fr;
   gap: 40px;
   img {
     max-width: 100%;
+    max-height: 200px;
+    display: block;
+    margin: 0 auto;
+  }
+  div:nth-child(1) {
+    order: 2;
+  }
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1.1fr 0.9fr;
+    div:nth-child(1) {
+      order: 0;
+    }
+    img {
+      max-width: 100%;
+    }
   }
 `;
-
 const Column = styled.div`
   display: flex;
   align-items: center;
 `;
-
 const ButtonsWrapper = styled.div`
   display: flex;
-  gap: 5px;
+  gap: 10px;
+  margin-top: 25px;
 `;
 
-export default function Featured() {
+export default function Featured({ product }) {
   return (
     <Bg>
       <Center>
         <ColumnsWrapper>
           <Column>
             <div>
-              <Title>Pro anywhere</Title>
-              <Desc>
-                Esse cupidatat anim irure magna aliquip enim velit occaecat. Ex
-                anim tempor proident duis. Dolor eu aliqua excepteur ut. Sunt
-                amet tempor laboris mollit commodo aliquip quis fugiat aute ex.
-                Aute exercitation eu deserunt tempor eiusmod ea adipisicing
-                eiusmod tempor culpa ullamco.
-              </Desc>
+              <Title>{product.title}</Title>
+              <Desc>{product.description}</Desc>
               <ButtonsWrapper>
-                <Button outline white size="l">
+                <ButtonLink href={"/products/" + product._id} $outline $white>
                   Read more
-                </Button>
-                <Button primary size="l">
+                </ButtonLink>
+                <Button $white>
+                  <CartIcon />
                   Add to cart
                 </Button>
               </ButtonsWrapper>
--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,52 @@
 import Link from "next/link";
-import { styled } from "styled-components";
-import Center from "./Center";
+import styled from "styled-components";
+import Center from "@/components/Center";
 
 const StyledHeader = styled.header`
   background-color: #222;
 `;
-
 const Logo = styled(Link)`
   color: #fff;
   text-decoration: none;
+  position: relative;
+  z-index: 3;
 `;
-
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 20px 0;
 `;
-
 const StyledNav = styled.nav`
-  display: flex;
+  ${(props) =>
+    props.mobileNavActive
+      ? `
+    display: block;
+  `
+      : `
+    display: none;
+  `}
   gap: 15px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 70px 20px 20px;
+  background-color: #222;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    position: static;
+    padding: 0;
+  }
 `;
-
 const NavLink = styled(Link)`
+  display: block;
   color: #aaa;
   text-decoration: none;
+  padding: 10px 0;
+  @media screen and (min-width: 768px) {
+    padding: 0;
+  }
 `;
 
 export default function Headers() {
