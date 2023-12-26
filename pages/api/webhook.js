70bcfe9b2e5aefe7867413d78b124fc80919623d
birkirfreyrg <birkirfrey@msn.com>
2023-12-26 10:36:19 +0000

    Added webhook for payment confirmation, sending data to stripe and getting data back.

---
+++ pages/api/webhook.js
@@ -0,0 +1,50 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_98a07761c907f9544cfe36550abe76f10a9e0c557aeb2719f27f89c4b0dbf186";
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  res.status(200).send("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+// superb-helped-zenith-fair
+// account id acct_1NucxrHq5XtcTa27
